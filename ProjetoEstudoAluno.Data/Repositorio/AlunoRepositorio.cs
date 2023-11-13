f7ec892214a3936ee5f2c01a8574552345ac6173
guga56 <gustavogregorio086@gmail.com>
2023-11-13 16:00:44 -0300

    desenvoolvendo o repositorio e sua interface com sua injeção de dependencia

---
+++ ProjetoEstudoAluno.Data/Repositorio/AlunoRepositorio.cs
@@ -0,0 +1,46 @@
+﻿using ProjetoEstudoAluno.Data.Contexto;
+using ProjetoEstudoAluno.Data.Repositorio.Interface;
+using ProjetoEstudoAluno.Dominio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstudoAluno.Data.Repositorio
+{
+    public class AlunoRepositorio : IAlunoRepositorio
+    {
+        private readonly BancoContexto _db;
+
+        public AlunoRepositorio(BancoContexto db)
+        {
+            _db = db;
+        }
+
+        public void AdiciionarAluno(Aluno aluno)
+        {
+            _db.Add(aluno);
+        }
+
+        public void Deletar(int id)
+        {
+            _db.Remove(id);
+        }
+
+        public List<Aluno> ListarAlunos()
+        {
+            return _db.Alunos.ToList();
+        }
+
+        public Aluno ListarPorId(int id)
+        {
+            return _db.Alunos.FirstOrDefault(x => x.IdCliente == id);
+        }
+
+        public void Update(Aluno aluno)
+        {
+            _db.Update(aluno);
+        }
+    }
+}
---
+++ ProjetoEstudoAluno.Data/Repositorio/Interface/IAlunoRepositorio.cs
@@ -0,0 +1,24 @@
+﻿using ProjetoEstudoAluno.Dominio.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoEstudoAluno.Data.Repositorio.Interface
+{
+    public interface IAlunoRepositorio
+    {
+        void AdiciionarAluno(Aluno aluno);
+
+        List<Aluno> ListarAlunos();
+
+        void Update(Aluno aluno);
+
+        Aluno ListarPorId(int id);
+
+        void Deletar(int id);
+
+
+    }
+}
